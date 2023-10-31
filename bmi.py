0b01cca52ad50f8f6fae2cdb023e189914d97e04
LRawrerL <yixuanliang05@gmail.com>
2023-10-31 09:11:11 +0800

    Add return values to BMI

--- bmi.py
+++ bmi.py
@@ -4,12 +4,17 @@ def calculate_bmi (height, weight):
     bmi = weight / (height ** 2)
     print("BMI = " + str(bmi))
 
+    return_value = 0
     if bmi <= 18.5:
         print("Underweight")
+        return_value = -1
     elif bmi <= 25:
         print("Normal")
+        return_value = 0
     elif bmi > 25:
         print("Overweight")
+        return_value = 1
+    return return_value
 
 
 calculate_bmi(1.73, 57)
